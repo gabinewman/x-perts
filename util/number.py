0f4e56815e4d0b92876f0ce80920ec99857ec4d6
darthbeep <darthbeep@gmail.com>
2018-10-13 23:15:18 -0400

    spaces and stuff

--- util/number.py
+++ util/number.py
@@ -13,22 +13,28 @@ class Number:
     def set_number(self, quantity=1, unit=""):
         self.numbers=[]
         self.add_item(quantity, unit)
+
     def append_number(self, number=1, unit=""):
         self.add_item(number, unit)
+
     def add_item(self, quantity, unit):
         self.numbers.append({"quantity":quantity, "unit": unit})
+
     def list_units(self):
         l = []
         for n in self.numbers:
             l.append(n["unit"])
         return l
+
     def remove_index(self, index):
         self.numbers.pop(index)
+
     def copy_number(self):
         copy = Number()
         for n in self.numbers:
             copy.numbers.append(n)
         return copy
+
     def __str__(self):
         ret=""
         for n in self.numbers:
@@ -36,6 +42,7 @@ class Number:
         if len(ret) > 3:
             return ret[:-3]
         return ""
+
     #Arithmetic operations on numbers
     def add(self, number):
         copy = self.copy_number()
@@ -48,15 +55,26 @@ class Number:
                     if toadd["unit"] == n["unit"]:
                         toadd["quantity"]+=n["quantity"]
         return copy
+
     def subtract(self, number):
         negative=Number()
         for n in number.numbers:
             negative.append_number(n["quantity"] * -1, n["unit"])
         return self.add(negative)
 
+    def multiply(self, number):
+        copy = self.copy_number()
+        for n in self.numbers:
+            for n2 in number.numbers:
+                copy.append_number(n["quantity"] * n2["quantity"], n["unit"] + n2["unit"])
+                print (copy.numbers[-1])
+        return copy
+
 n1=Number(4, "x")
 n1.append_number(4)
-n2=Number(8)
-n2.append_number(7, "y")
-n1.subtract(n2)
-print(n1)
+n2=Number(4)
+n2.append_number(4, "y")
+#n1.subtract(n2)
+#print(n1.numbers)
+#print(n2.numbers)
+print(n1.multiply(n2))
