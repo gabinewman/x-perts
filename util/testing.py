71c0fb0668e4ce0889aac44612171fa636beb2f5
Anshul Nagar <nagara2@1x-nat-vl930-172-30-111-110.wireless.umass.edu>
2018-10-13 22:45:42 -0400

    Added testing

---
+++ util/testing.py
@@ -0,0 +1,14 @@
+string_to_test = "3 + 5+ * 10";
+string_split = string_to_test.split();
+variable = [];
+
+for element in string_split:
+    try:
+        i = float(element);
+        variable.append(i);
+        print(variable);
+    except:
+        variable.append(element);
+
+
+print(variable);
